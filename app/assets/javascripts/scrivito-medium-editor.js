69930509d752840114c99e7774a170c86077bffe
David Császár <dcsaszar@users.noreply.github.com>
2015-07-21 09:41:35 +0200

    Simplify defaults

--- app/assets/javascripts/scrivito-medium-editor.js
+++ app/assets/javascripts/scrivito-medium-editor.js
@@ -4,26 +4,14 @@
 ;(function() {
 
 scrivito.on('content', function(content) {
-  if (!scrivito.in_editable_view()) { return; }
+  if (!scrivito.in_editable_view()) {
+    return;
+  }
 
   $(content).find('[data-editor=medium]').each(function() {
     var contenteditable = $(this);
 
-    var config;
-    if (contenteditable.attr('data-medium-editor')) {
-      config = JSON.parse(contenteditable.attr('data-medium-editor'));
-    } else {
-      config = {buttons: [
-        'bold',
-        'italic',
-        'underline',
-        'header1',
-        'header2',
-        'unorderedlist',
-        'orderedlist'
-      ]};
-    }
-
+    var config = contenteditable.data('medium-editor');
     new MediumEditor(contenteditable, config);
 
     contenteditable.on('input', function() {
